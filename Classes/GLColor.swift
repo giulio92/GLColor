28c5510df1f3fa1da130c48cac11aaee0dd42f54
Giulio Lombardo <giulio.lombardo@gmail.com>
2017-11-02 20:09:01 +0100

    Minor

--- Classes/GLColor.swift
+++ Classes/GLColor.swift
@@ -35,6 +35,7 @@ public final class GLColor: UIColor {
 		switch gamut {
 		case .P3:
 			self.init(displayP3Red: red / 255.0, green: green / 255.0, blue: blue / 255.0, alpha: alpha)
+			
 		default:
 			self.init(red: red / 255.0, green: green / 255.0, blue: blue / 255.0, alpha: alpha)
 		}
